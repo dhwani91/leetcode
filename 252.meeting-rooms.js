3eff19ddb954058693062e29891f1324a68a3333
Dhwani Sanghvi <dhwani.sanghvi91@gmail.com>
2022-04-03 05:28:24 -0700

    meeting room 2

--- 252.meeting-rooms.js
+++ 252.meeting-rooms.js
@@ -20,3 +20,25 @@
  * @param {Interval[]} intervals
  * @return {boolean}
  */
+
+ var canAttendMeetings = function(intervals) {
+    intervals.sort((a,b) => {
+        return a.start > b.start ? 1 : -1;
+    });
+
+    console.log("intervals", intervals)
+    
+    for(var i=1; i < intervals.length; i++) {
+        console.log("end", intervals[i-1][1])
+        console.log("start",  intervals[i][0])
+        if(intervals[i - 1].end > intervals[i].start) {
+            return false;
+        }
+    }
+    
+    return true;
+};
+
+ console.log("result",canAttendMeetings([[1,30], [11,40], [5,10]]))
+ console.log("result",canAttendMeetings([[1,3],[5,10], [11,40]]))
+
